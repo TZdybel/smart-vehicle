da46ea5ea2df828f28f1c8d75a9ad45143bffa46
logx <pkardas.it@gmail.com>
2019-01-07 19:35:57 +0100

    added keep alive status

--- wifi-controller/src/main.py
+++ wifi-controller/src/main.py
@@ -1,20 +1,6 @@
 import socket
 import pygame
-import time
-
-
-def init_wifi(host, port):
-    # host_addr = '198.168.0.1'
-    # port = 2137
-    # sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    # sock.connect((device_mac_addr, port))
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((host, port))
-
-    time.sleep(2)
-
-    return s
+from time import sleep
 
 
 def init_pygame():
@@ -32,28 +18,28 @@ def read_keyboard():
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
-        print("SPEED UP")
+        # print("SPEED UP")
         data[0] = '1'
     elif keys[pygame.K_s]:
-        print("SLOW DOWN")
+        # print("SLOW DOWN")
         data[1] = '1'
 
     if keys[pygame.K_UP]:
-        print("FORWARD")
+        # print("FORWARD")
         data[2] = '1'
     elif keys[pygame.K_DOWN]:
-        print("BACKWARD")
+        # print("BACKWARD")
         data[3] = '1'
 
     if keys[pygame.K_LEFT]:
-        print("LEFT")
+        # print("LEFT")
         data[4] = '1'
     elif keys[pygame.K_RIGHT]:
-        print("RIGHT")
+        # print("RIGHT")
         data[5] = '1'
 
     if keys[pygame.K_ESCAPE]:
-        print("STOPPING")
+        # print("STOPPING")
         data = None
 
     return ''.join(data)
@@ -62,13 +48,17 @@ def read_keyboard():
 def process():
     init_pygame()
 
-    # host = '192.168.10.1'
+    # host = '192.168.43.94'
     host = "127.0.0.1"
     port = 8002
-    sock = init_wifi(host, port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect((host, port))
+    last_sent = None
+
+    counter = 0
 
     while True:
-        # pygame.time.wait(500)
+        counter += 1
 
         data = read_keyboard()
 
@@ -76,8 +66,16 @@ def process():
             sock.close()
             return
 
-        print(data)
-        sock.sendto(data.encode(), (host, port))
+        if data != last_sent:
+            counter = 0
+            sock.sendto(data.encode(), (host, port))
+            print("CHANGE ", data)
+            last_sent = data
+        elif counter > 1000000:
+            counter = 0
+            sock.sendto(data.encode(), (host, port))
+            print("KEEP ALIVE ", data)
+
         pygame.event.pump()
 
 
